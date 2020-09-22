297b4299cf48922a57a27c7de4983def3310acf2
Wei Zhang <iusenenow@gmail.com>
2020-09-22 16:59:20 +1000

    update

--- index.js
+++ index.js
@@ -30,19 +30,31 @@ const resultsWrapper = document.querySelector('.results')
 const onInput = async e => {
   const movies = await fetchData(e.target.value)
 
+  if (!movies?.length) {
+    dropdown.classList.remove('is-active')
+    return
+  }
+
   resultsWrapper.innerHTML = ''
 
   dropdown.classList.add('is-active')
 
   for (let movie of movies) {
     const option = document.createElement('a')
+
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+
     option.classList.add('dropdown-item')
 
     option.innerHTML = `
       <img src="${imgSrc}" />
       ${movie.Title}
     `
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active')
+      input.value = movie.Title
+      onMovieSelect(movie)
+    })
 
     resultsWrapper.appendChild(option)
   }
@@ -55,3 +67,33 @@ document.addEventListener('click', e => {
     dropdown.classList.remove('is-active')
   }
 })
+
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com', {
+    params: {
+      apikey: 'f6aceaef',
+      i: movie.imdbID
+    }
+  })
+
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+}
+
+const movieTemplate = movieDetail => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+  `
+}
