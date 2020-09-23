28738444f2e5fd9822f50c48f4c5d378b370c86f
Wei Zhang <iusenenow@gmail.com>
2020-09-23 16:36:52 +1000

    change http to https

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const autoCompleteConfig = {
     return movie.Title
   },
   async fetchData(searchTerm) {
-    const response = await axios.get('http://www.omdbapi.com', {
+    const response = await axios.get('https://www.omdbapi.com', {
       params: {
         apikey: 'f6aceaef',
         s: searchTerm
@@ -46,7 +46,7 @@ createAutoComplete({
 let leftMovie
 let rightMovie
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get('http://www.omdbapi.com', {
+  const response = await axios.get('https://www.omdbapi.com', {
     params: {
       apikey: 'f6aceaef',
       i: movie.imdbID
